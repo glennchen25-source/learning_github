507715301f5fb905b8ee3747a68a3322a78bd01b
glennchen25-source <glennchen25@gmail.com>
2025-10-28 00:48:15 +0800

    修改网络无法同步的问题

--- use_git_2.sh
+++ use_git_2.sh
@@ -3,8 +3,13 @@ git clone "仓库链接，https即可”
 #密码是token，在备忘录里面
 
 #增改删操作,步骤见use_git_1
+git config --global http.version HTTP/1.1
+#我们不用http2.2的网，可能跟VPN有关
+git push 
 
-git push #将本地同步到远程
+5)（可选）恢复默认 HTTP/2
+若以后网络环境正常想恢复：
+git config --global --unset http.version
 
 git config --global user.name ''
 git config --global user.email ''
